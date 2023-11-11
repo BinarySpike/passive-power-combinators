92f2f7419efccdee85b8e08f51e5330c6673b306
BinarySpike <binaryspike@gmail.com>
2023-11-11 16:27:07 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Passive Power Combinators
+
+A mod for the game Factorio.
+
+Removes the power requirements for combinators and programmable speaker
---
+++ data-updates.lua
@@ -0,0 +1,19 @@
+data.raw["arithmetic-combinator"]["arithmetic-combinator"].energy_source = { type = "void" }
+data.raw["decider-combinator"]["decider-combinator"].energy_source = { type = "void" }
+data.raw["programmable-speaker"]["programmable-speaker"].energy_source = { type = "void" }
+
+-- data:extend
+-- {
+--   {
+--     name = "arithmetic-combinator",
+--     energy_source = { type = "void" }
+--   },
+--   {
+--     name = "decider-combinator",
+--     energy_source = { type = "void" }
+--   },
+--   {
+--     name = "programmable-speaker",
+--     energy_source = { type = "void" }
+--   }
+-- }
